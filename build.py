cb4a2275d43e2d6ffc098bf958e96fcf1f46b538
Charlie Jiang <cqjjjzr@126.com>
2019-08-30 17:33:11 +0800

    update CI files

--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from cpt.packager import ConanMultiPackager
 
+
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(pure_c=True)
+
+    builder = build_template_default.get_builder()
+
     builder.run()
